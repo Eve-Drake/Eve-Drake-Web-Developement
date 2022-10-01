46f0bcf65ca8e0fe5a2fbc3efae7cd1447d6b522
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-10-01 20:40:01 +0100

    start of email contact functions

--- components/ContactMe.js
+++ components/ContactMe.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 const ContactMe = () => {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('')
+  const [contactEmail, setContactEmail] = useState('')
+  const [email, setEmail] = useState('')
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmail(`Message from ${name} that can be contacted at ${contactEmail}. It states ${message}`)
+    emailjs.send('service_6pp6vos', 'template_638wrgd', {email}, 'EKQBp2Lom6XwoAEhP');
+    setName('')
+    setEmail('')
+    setContactEmail('')
+  }
   return (
-    <div className='bg-gradient-to-b from-GREY to-LIGHT h-screen'>
-        
+    <div>
+      <h1 className='text-ORANGE'>Contact <span className='text-BLUE'>Me</span></h1>
+    <form onSubmit={handleSubmit}>
+      <label>Name</label>
+      <input 
+        type="text" 
+        name="name"
+        value={name} 
+        onChange={(e)=>setName(e.target.value)}/>
+
+      <label>Your Email</label>
+      <input 
+        type="text" 
+        name="emailname"
+        value={contactEmail} 
+        onChange={(e)=>setContactEmail(e.target.value)}/>
+      
+      <label>Message</label>
+      <textarea 
+        name="message"
+        value={message} 
+        onChange={(e)=>setMessage(e.target.value)}/>
+      <button type='submit'>Sent</button>
+    </form>
     </div>
   )
 }
--- components/DisplayProjects.js
+++ components/DisplayProjects.js
@@ -3,21 +3,20 @@ import React from 'react'
 const DisplayProjects = () => {
     const projectArray = [
         {id:0, title: 'Blog With Pagination', type:'JavaScript', img: '', link: 'https://github.com/Eve-Drake/blog-with-pagination'},
-        {id:1, title: 'TypeScript API Call', type:'TypeScript & API Calls', img: '', link: 'https://github.com/Eve-Drake/Typescript-Api-User-database'},
-        {id:2, title: 'TypeScript Todo List', type:'TypeScript & Redux Toolkit', img: '/../public/ProjectImages/ReducTypescriptTodo.jpg', link: 'https://github.com/Eve-Drake/Typescript-Todo-List'},
-        {id:3, title: 'TypeScript Redux CRUD', type:'TypeScript & Redux Toolkit', img: '', link: 'https://github.com/Eve-Drake/Typescript-Redux-CRUD'},
+        {id:1, title: 'TypeScript API Call', type:'TypeScript', img: '', link: 'https://github.com/Eve-Drake/Typescript-Api-User-database'},
+        {id:2, title: 'TypeScript Todo List', type:'TypeScript', img: '/../public/ProjectImages/ReducTypescriptTodo.jpg', link: 'https://github.com/Eve-Drake/Typescript-Todo-List'},
+        {id:3, title: 'TypeScript Redux CRUD', type:'TypeScript', img: '', link: 'https://github.com/Eve-Drake/Typescript-Redux-CRUD'},
         {id:4, title: 'Random User Generator', type:'JavaScript', img: '', link: 'https://github.com/Eve-Drake/Random-User-Generator'},
-        {id:5, title: 'Bootstrap Placeholder Reviews', type:'JavaScript & Bootstrap', img: '', link: 'https://github.com/Eve-Drake/Bootstrap-Placeholder-Reviews'},
-        {id:6, title: 'Data Visulisation with ReCharts', type:'JavaScript & ReCharts', img: '', link: 'https://github.com/Eve-Drake/Data-Visulisation'},
-        {id:7, title: 'JavaScript Simple Todo', type:'JavaScript & API Calls', img: '', link: 'https://github.com/Eve-Drake/Simple-Todo'},
+        {id:5, title: 'Bootstrap Placeholder Reviews', type:'JavaScript', img: '', link: 'https://github.com/Eve-Drake/Bootstrap-Placeholder-Reviews'},
+        {id:6, title: 'Data Visulisation with ReCharts', type:'JavaScript', img: '', link: 'https://github.com/Eve-Drake/Data-Visulisation'},
+        {id:7, title: 'JavaScript Simple Todo', type:'JavaScript', img: '', link: 'https://github.com/Eve-Drake/Simple-Todo'},
     ]
   return (
     <div className='grid md:grid-cols-4 gap-4 p-10'>
         {projectArray.map((project)=>(
             <div key={project.id}>
                 <div className="max-w-sm rounded  shadow-md hover:shadow-xl">
-                    <img className="w-full" src='../public/ProjectImages/DataVis.jpg' alt="Project Image" />
-                    <div className="px-6 py-4 h-500 min-h-full">
+                    <div className="px-6 py-4 h-40 min-h-full">
                         <div className="font-bold text-xl mb-2 ">
                             <h3 className='text-center'><a href={project.link}>{project.title}</a></h3>
                         </div>
